8dc7a63d8e1864d01997cc91b01d01b12a99234f
Cliff Canan <cliff@nooch.com>
2016-01-28 00:55:15 -0500

    • Lots of layout updates for Admin view of Building page while editing; • Fixed remaining issue with fg-line on input blur

--- client/fg-line.js
+++ client/fg-line.js
@@ -4,28 +4,20 @@ $(document).ready(function ()
 {
     console.log("Fg-Line.js -> document is ready");
 
-    $('body').on('focus', '.form-control', function ()
+    $('body').on('focus', '.form-control.fg-input', function ()
     {
         console.log("FG-LINE - Checkpoint #1 - Input Focused")
         $(this).closest('.fg-line').addClass('fg-toggled');
     })
 
-    $('body').on('blur', '.form-control', function ()
+    $('body').on('blur', '.form-control.fg-input', function ()
     {
         console.log("FG-LINE - checkpoint 2 - input blurred")
 
-        var fgrp = $(this).closest('.form-group');
-        var ipgrp = $(this).closest('.input-group');
-
         var val = $(this).val();
 
-        if (fgrp.hasClass('fg-float') && val.length == 0) {
-            $(this).closest('.fg-line').removeClass('fg-toggled');
-        }
-        else if (ipgrp.hasClass('fg-float') && val.length == 0) {
-            $(this).closest('.fg-line').removeClass('fg-toggled');
-        }
-        else {
+        if (val.length == 0 && $(this).closest('.fg-line').hasClass('fg-toggled'))
+        {
             $(this).closest('.fg-line').removeClass('fg-toggled');
         }
     });
